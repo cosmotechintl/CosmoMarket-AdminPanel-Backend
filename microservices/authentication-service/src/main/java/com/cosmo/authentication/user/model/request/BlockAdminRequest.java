4ffa26473458b4f25a0d50f1da99c5f4f0004f4e
Manjul Tamang <manzultamang777@gmail.com>
2024-06-12 12:16:55 +0545

    admin user edit, delete and block api created

---
+++ microservices/authentication-service/src/main/java/com/cosmo/authentication/user/model/request/BlockAdminRequest.java
@@ -0,0 +1,11 @@
+package com.cosmo.authentication.user.model.request;
+
+import com.cosmo.common.model.ModelBase;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BlockAdminRequest extends ModelBase {
+    private Long id;
+}
---
+++ microservices/authentication-service/src/main/java/com/cosmo/authentication/user/model/request/UpdateAdminRequest.java
@@ -0,0 +1,24 @@
+package com.cosmo.authentication.user.model.request;
+
+import com.cosmo.authentication.accessgroup.model.AccessGroupDto;
+import com.cosmo.common.model.ModelBase;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateAdminRequest extends ModelBase {
+
+        private Long id;
+        @NotBlank(message = "Name cannot be blank")
+        private String name;
+        @NotBlank(message = "Mobile Number cannot be null")
+        private String mobileNumber;
+        @NotBlank(message = "Address cannot be null")
+        private String address;
+        @NotNull
+        private AccessGroupDto accessGroup;
+
+}
