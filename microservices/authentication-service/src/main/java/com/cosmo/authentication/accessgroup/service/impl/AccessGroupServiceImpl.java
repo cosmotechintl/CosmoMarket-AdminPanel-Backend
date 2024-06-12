1dcc146e2d11c049edd2580712d5280567ec0a84
Manjul Tamang <manzultamang777@gmail.com>
2024-06-12 12:37:39 +0545

    access group edit api created

--- microservices/authentication-service/src/main/java/com/cosmo/authentication/accessgroup/service/impl/AccessGroupServiceImpl.java
+++ microservices/authentication-service/src/main/java/com/cosmo/authentication/accessgroup/service/impl/AccessGroupServiceImpl.java
@@ -4,7 +4,6 @@ import com.cosmo.authentication.accessgroup.mapper.AccessGroupMapper;
 import com.cosmo.authentication.accessgroup.mapper.AccessGroupRoleMapMapper;
 import com.cosmo.authentication.accessgroup.model.CreateAccessGroupModel;
 import com.cosmo.authentication.accessgroup.model.SearchAccessGroupResponse;
-import com.cosmo.authentication.accessgroup.model.request.DeleteAccessGroupRequest;
 import com.cosmo.authentication.accessgroup.model.request.FetchAccessGroupDetail;
 import com.cosmo.authentication.accessgroup.model.request.UpdateAccessGroupRequest;
 import com.cosmo.authentication.accessgroup.service.AccessGroupService;
@@ -16,7 +15,6 @@ import com.cosmo.common.model.ApiResponse;
 import com.cosmo.common.model.PageableResponse;
 import com.cosmo.common.model.SearchParam;
 import com.cosmo.common.model.SearchResponseWithMapperBuilder;
-import com.cosmo.common.repository.StatusRepository;
 import com.cosmo.common.service.SearchResponse;
 import com.cosmo.common.util.ResponseUtil;
 import jakarta.transaction.Transactional;
@@ -34,7 +32,6 @@ public class AccessGroupServiceImpl implements AccessGroupService {
     private final AccessGroupRoleMapMapper accessGroupRoleMapMapper;
     private final SearchResponse searchResponse;
     private final AccessGroupSearchRepository accessGroupSearchRepository;
-    private final StatusRepository statusRepository;
 
 
     @Override
@@ -53,27 +50,15 @@ public class AccessGroupServiceImpl implements AccessGroupService {
         if (existedAccessGroup.isPresent()){
             AccessGroup updatedAccessGroup= accessGroupMapper.updateAccessGroup(request,existedAccessGroup.get());
             accessGroupRoleMapMapper.updateAccessGroupRoleMap(updatedAccessGroup,request.getRoles());
-            return Mono.just(ResponseUtil.getSuccessfulApiResponse("Access group updated successfully"));
+            return Mono.just(ResponseUtil.getSuccessfulApiResponse("access group updated successfully"));
             }
         else {
-            return Mono.just(ResponseUtil.getNotFoundResponse("Access group not found"));
+            return Mono.just(ResponseUtil.getNotFoundResponse("access group not found"));
     }}
 
     @Override
-    public Mono<ApiResponse<?>> deleteAccessGroup(DeleteAccessGroupRequest deleteAccessGroupRequest) {
-        Optional<AccessGroup> accessGroup= accessGroupRepository.findById(deleteAccessGroupRequest.getId());
-        if (accessGroup.isEmpty()){
-            return Mono.just(ResponseUtil.getNotFoundResponse("Access group not found"));
-        }
-        else{
-            AccessGroup accessGroup1= accessGroup.get();
-            if ("DELETED".equals(accessGroup1.getStatus().getName())){
-                return Mono.just(ResponseUtil.getNotFoundResponse("Access group not found"));
-            }
-            accessGroup1.setStatus(statusRepository.findByName("DELETED"));
-            accessGroupRepository.save(accessGroup1);
-            return Mono.just(ResponseUtil.getSuccessfulApiResponse("Access group deleted successfully"));
-        }
+    public void deleteAccessGroup(Long id) {
+
     }
 
     @Override
@@ -96,7 +81,7 @@ public class AccessGroupServiceImpl implements AccessGroupService {
             return Mono.just(ResponseUtil.getNotFoundResponse("Access group  not found"));
         } else {
             AccessGroupDetailDto accessGroupDetailDto = accessGroupMapper.getAccessGroupDetailDto(accessGroup.get());
-            return Mono.just(ResponseUtil.getSuccessfulApiResponse(accessGroupDetailDto, "Access group fetched successfully"));
+            return Mono.just(ResponseUtil.getSuccessfulApiResponse(accessGroupDetailDto, "access group fetched successfully"));
         }
     }
 
